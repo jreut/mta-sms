3141e0eb4439d377bd1a363bdf77ecd93bf70b4a
Jordan Ryan Reuter <oss@jreut.com>
2018-08-11 23:00:44 -0400

    un-nest POST /search/twilio route

--- lib/app.rb
+++ lib/app.rb
@@ -17,21 +17,19 @@ end
 
 class App < Roda
   route do |r|
-    r.on 'search' do
+    r.post 'search/twilio' do
       # the MTA Web site always expects this time zone
       tz = TZInfo::Timezone.get 'America/New_York'
       @now = tz.utc_to_local Time.now.utc
 
-      r.post 'twilio' do
-        response['Content-Type'] = 'text/plain'
-        origin, destination = r.params['Body'].split(%r{[./]}).map(&:strip)
-        TIMETABLE.(
-          origin: origin, destination: destination,
-          time: @now,
-        ).fmap do |timetable|
-          TimetablePresenter.new(timetable: timetable, now: @now).call
-        end.value_or(&:itself)
-      end
+      response['Content-Type'] = 'text/plain'
+      origin, destination = r.params['Body'].split(%r{[./]}).map(&:strip)
+      TIMETABLE.(
+        origin: origin, destination: destination,
+        time: @now,
+      ).fmap do |timetable|
+        TimetablePresenter.new(timetable: timetable, now: @now).call
+      end.value_or(&:itself)
     end
   end
 end
